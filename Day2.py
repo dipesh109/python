286b5b062b2a1d4b220fbb87d598e6ddfd7c5f1d
dipesh109 <marasinidipesh@gmail.com>
2025-11-12 18:41:24 +0545

    day2

--- Day2.py
+++ Day2.py
@@ -1,2 +1,49 @@
 num = 3
+num1 = 3.14
 print(type(num))
+print(type(num1))
+
+
+#Arthimetic Operator
+#addition (+)
+print(3+2)
+#subtraction (-)
+print(3-2)
+#Multiplication(*)
+print(3*2)
+#division(/)
+print(3/2)
+# floor division(//)
+print(3//2)
+#exponent(**)
+print(3**2)
+# modulus(%)
+print(5%2)
+
+num3 =1 ;
+num3 = num3+1;
+num3 += 1;
+print(num3)
+
+num4 = -150
+print(abs(num4))
+
+#round
+print(round(3.75));
+print(round(3.75,1));
+
+# comprasion
+num1 = 3;
+num2 = 6;
+print(num1 == num2);
+print(num1 != num2);
+print(num1> num2);
+print(num1 < num2);
+
+
+num3 = '100';
+num4 = '200';
+print(num3+num4)
+num3 = int(num3);
+num4 = int(num4)
+print(num3 + num4)
